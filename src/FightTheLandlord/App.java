cfd4e4a5efd984e922fc3c553cd22a9bf816dc9e
luos81 <13507889510@163.com>
2026-02-25 17:20:31 +0800

    版本1.1完成了基础的功能（控制台版） 1：牌盒的创建 2：洗牌的功能 3：发牌的功能 4：看牌的功能

--- src/FightTheLandlord/App.java
+++ src/FightTheLandlord/App.java
@@ -1,4 +1,21 @@
 package FightTheLandlord;
 
 public class App {
+    public static void main(String[] args) {
+        /*
+            完成控制台版本的三种：
+            准备牌
+            洗牌
+            发牌
+         */
+
+        //启动游戏
+        new PokerGame();
+
+
+
+
+
+
+    }
 }
--- src/FightTheLandlord/PokerGame.java
+++ src/FightTheLandlord/PokerGame.java
@@ -1,4 +1,85 @@
 package FightTheLandlord;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class PokerGame {
+
+    // 牌盒
+    static ArrayList<String> list = new ArrayList<>();
+
+    static {
+        // 准备牌
+        // 花色："♠","♥","♦","♣"
+        // 数字："3","4","5","6","7","8","9","10","J","Q","K","A","2"
+        String[] color = {"♦", "♣", "♥", "♠"};
+        String[] number = {"3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A", "2"};
+
+        //放入牌盒
+        for (String c : color) {
+            // c表示花色
+            for (String n : number) {
+                // n表示数字
+                list.add(c + n);
+            }
+        }
+        list.add("大王");
+        list.add("小王");
+
+    }
+
+
+    public PokerGame() {
+
+        // 洗牌
+        Collections.shuffle(list);
+
+        // 发牌
+        ArrayList<String> lord = new ArrayList<>();
+        ArrayList<String> player1 = new ArrayList<>();
+        ArrayList<String> player2 = new ArrayList<>();
+        ArrayList<String> player3 = new ArrayList<>();
+
+        // 遍历每一张牌
+        for (int i = 0; i < list.size(); i++) {
+            String poker = list.get(i);
+
+            // 存储三张底牌(将前面三张底牌发给lord存储)
+            if (i <= 2) {
+                lord.add(poker);
+                continue;
+            }
+
+            // 给三个玩家轮流发牌
+            if (i % 3 == 0) {
+                player1.add(poker);
+            } else if (i % 3 == 1) {
+                player2.add(poker);
+            } else {
+                player3.add(poker);
+            }
+        }
+
+
+        //看牌
+        lookPoker("底牌", lord);
+        lookPoker("玩家一", player1);
+        lookPoker("玩家二", player2);
+        lookPoker("玩家三", player3);
+    }
+
+    /*
+        参数一：玩家的名字
+        参数二：每位玩家的牌
+     */
+    public void lookPoker(String name, ArrayList<String> list) {
+        System.out.println(name + "：");
+
+        for (String poker : list) {
+            System.out.print(poker + " ");
+        }
+        System.out.println();
+
+    }
+
 }
